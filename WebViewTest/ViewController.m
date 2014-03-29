539be81b2f60b133d4056d77e1a5b1b23332c65b
Joe Bologna <joebologna@mac.com>
2014-03-29 11:19:02 -0500

    working webview without status bar

--- WebViewTest/ViewController.m
+++ WebViewTest/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate> {
+    UIWebView *webView;
+}
 
 @end
 
@@ -17,13 +19,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    webView = [[UIWebView alloc]initWithFrame:self.view.frame];
+    //webView.scrollView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0);
+    webView.delegate = self;
+    NSString *file = @"index.html";
+    NSURL *url = [NSURL URLWithString:file relativeToURL:[[NSBundle mainBundle] bundleURL]];
+    NSLog(@"%@", url.path);
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+    
+    [self.view addSubview:webView];
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSLog(@"%s", __func__);
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    NSLog(@"%s: %@", __func__, error);
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSLog(@"%s", __func__);
+    return YES;
 }
 
 @end
